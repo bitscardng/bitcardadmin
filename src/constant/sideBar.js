2a0fac5baedd617046d163d85d1ce54d6b59cfc5
Oke Babatunde <103512075+Twise007@users.noreply.github.com>
2023-05-18 23:31:41 +0100

    sidebar data

---
+++ src/constant/sideBar.js
@@ -0,0 +1,32 @@
+
+
+
+const menu = [
+  { name: "dashboard", link: "/dashboard", amount: "" },
+  { name: "admin control", link: "/admin-control", amount: "" },
+  { name: "gift card tranx", link: "/gift-card-tranx", amount: "" },
+  { name: "P2P tranx", link: "/p2p-tranx", amount: "" },
+  { name: "virtual card", link: "/virtual-card", amount: "" },
+  { name: "crypto tranx", link: "/crypto-tranx", amount: "" },
+  { name: "users", link: "/users", amount: "" },
+  { name: "news", link: "/news", amount: "" },
+  { name: "FAQ", link: "/FAQ", amount: "" },
+  { name: "USD withdrawal", link: "/USD-withdrawal", amount: "" },
+  { name: "NGN withdrawal", link: "/NGN-withdrawal", amount: "" },
+  { name: "GHS withdrawal", link: "/GHS-withdrawal", amount: "" },
+  { name: "USD withdrawal", link: "/USD-withdrawal", amount: "" },
+  { name: "FX Rate", link: "/FX-Rate", amount: "" },
+  { name: "Crypto Rate", link: "/Crypto-Rate", amount: "" },
+  { name: "Gift Card Rate", link: "/Gift-Card-Rate", amount: "" },
+  { name: "NGN Deposit", link: "/NGN-deposit", amount: "" },
+  { name: "USD Deposit", link: "/USD-deposit", amount: "" },
+  { name: "NGN transfer", link: "/NGN-transfer", amount: "" },
+  { name: "USD transfer", link: "/USD-transfer", amount: "" },
+  { name: "push notification", link: "/push-notification", amount: "" },
+  { name: "export data", link: "/export-data", amount: "" },
+  { name: "ads campaign", link: "/ads-campaign", amount: "" },
+  { name: "send email BC", link: "/send-email-BC", amount: "" },
+  { name: "KYC 1 & 2", link: "/KYC-1&2", amount: "" },
+  { name: "KYC 3", link: "/KYC-3", amount: "" },
+  { name: "KYC 4", link: "/KYC-4", amount: "" },
+];

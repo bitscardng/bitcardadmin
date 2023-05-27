7618d4b050d71a9d8844e9533dfae1e59869c4bc
Oke Babatunde <103512075+Twise007@users.noreply.github.com>
2023-05-27 03:07:28 +0100

    crypto rate

--- src/pages/CryptoRate.jsx
+++ src/pages/CryptoRate.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
+import Search from "../components/Search";
 
-const CryptoRate = () => {
+const rate = [
+  { coin: "BTC", buying: "749.5", selling: "770", market: "29,012.23" },
+  { coin: "BTC", buying: "747.5", selling: "774", market: "28,000.23" },
+];
+
+const GhsWithdraw = () => {
+  const [search, setSearch] = useState("");
+
+  //pagination end
+
   return (
-    <div>CryptoRate</div>
-  )
-}
+    <div>
+      <div className="p-2 pt-8">
+        <div className="flex justify-between w-full gap-8 overflow-x-auto">
+          <div className="w-full overflow-x-auto">
+            <div className="flex items-center gap-20 pb-2 text-center">
+              <Search
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+              />
+              <p className="text-2xl font-bold text-end">Crypto Rate</p>
+            </div>
+            <table className="w-full ">
+              {/* head */}
+              <thead className="">
+                <tr className="rounded-full">
+                  <th className="p-2 text-xl font-semibold border"></th>
+                  <th className="p-2 text-xl font-semibold border">Coin</th>
+                  <th className="p-2 text-xl font-semibold border">Buying</th>
+                  <th className="p-2 text-xl font-semibold border">Selling</th>
+                  <th className="p-2 text-xl font-semibold border">
+                    Market Rate $
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {rate.map((data, index) => {
+                  return (
+                    <tr className="text-center" key={index}>
+                      <th className="p-1 px-2 text-xl duration-500 border">
+                        {index + 1}
+                      </th>
+                      <td className="p-1 px-2 text-xl font-thin duration-500 border">
+                        {data.coin}
+                      </td>
+                      <td className="p-1 px-2 text-xl font-thin duration-500 border">
+                        {data.buying}
+                      </td>
+                      <td className="p-1 px-2 text-xl font-thin duration-500 border">
+                        {data.selling} / $
+                      </td>
+                      <td className="p-1 px-2 text-xl font-thin duration-500 border">
+                        {"29.012.23"}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CryptoRate
+export default GhsWithdraw;
--- src/pages/UsdWithdraw.jsx
+++ src/pages/UsdWithdraw.jsx
@@ -1,9 +1,151 @@
-import React from 'react'
+import React, { useState } from "react";
+import Search from "../components/Search";
+import { giftCard } from "../constant";
+import ReactPaginate from "react-paginate";
+import { HiOutlineMail } from "react-icons/hi";
 
 const UsdWithdraw = () => {
-  return (
-    <div>UsdWithdraw</div>
-  )
-}
+  const [datas, setDatas] = useState(giftCard);
+  const [search, setSearch] = useState("");
 
-export default UsdWithdraw
+  // paginations start
+  const [pageNumber, setPageNumber] = useState(0);
+  //data view page is datasperpage so you can change the number 5 to what you want...
+  const datasPerPage = 5;
+  const pageVisited = pageNumber * datasPerPage;
+  const pageCount = Math.ceil(datas.length / datasPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+  const displayDatas = datas
+    .slice(pageVisited, pageVisited + datasPerPage)
+    .map((data, index) => {
+      return (
+        <tr className="text-center" key={index}>
+          <th className="p-1 px-2 text-xl duration-500 border">{index + 1}</th>
+          <td className="p-1 px-2 text-xl font-thin duration-500 border">
+            {data.email}
+          </td>
+          <td className="p-1 px-2 text-xl font-thin duration-500 border">
+            {data.name}
+          </td>
+          <td className="p-1 px-2 text-xl font-thin duration-500 border">
+            {data.amount}
+          </td>
+          <td className="p-1 px-2 text-xl font-thin duration-500 border">
+            <p className="p-2 rounded-lg bg-active">Preview details</p>
+          </td>
+          <td className="p-1 px-2 text-xl font-thin duration-500 border">
+            {data.transDate} <span>{data.transTime}</span>
+          </td>
+          <td className="flex flex-col h-full gap-2 p-2 text-xl font-thin border">
+            <btn className="bg-[green] p-1 rounded-lg cursor-pointer hover:font-normal duration-500">
+              Approved
+            </btn>
+            <btn className="bg-[red] p-1 rounded-lg cursor-pointer hover:font-normal duration-500">
+              Decline
+            </btn>
+          </td>
+        </tr>
+      );
+    });
+  //pagination end
+
+  return (
+    <div>
+      <div className="p-2 pt-8">
+        <div className="flex justify-between w-full gap-8 overflow-x-auto">
+          <div className="w-full overflow-x-auto">
+            <div className="flex items-center gap-20 pb-2 text-center">
+              <Search
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+              />
+              <p className="text-2xl font-bold text-end">Usd Withdraw</p>
+            </div>
+            <table className="w-full ">
+              {/* head */}
+              <thead className="">
+                <tr className="rounded-full">
+                  <th className="p-2 text-xl font-semibold border"></th>
+                  <th className="p-2 text-xl font-semibold border">Email</th>
+                  <th className="p-2 text-xl font-semibold border">Username</th>
+                  <th className="p-2 text-xl font-semibold border">Amount</th>
+                  <th className="p-2 text-xl font-semibold border">Details</th>
+                  <th className="p-2 text-xl font-semibold border">Date</th>
+                  <th className="p-2 text-xl font-semibold border">Status</th>
+                </tr>
+              </thead>
+              <tbody>{displayDatas}</tbody>
+            </table>
+          </div>
+          <div className="flex flex-col w-[28rem]">
+            <p className="pb-2 text-2xl font-bold text-center">
+              User Transaction
+            </p>
+            <div className="flex flex-col font-thin text-[#9395A4]">
+              <div className="flex items-center w-full gap-3 p-2 px-4 my-2 rounded-full bg-sec">
+                <div className="flex flex-row items-center gap-2 text-center">
+                  <div className="p-2 text-center rounded-full bg-[#3b3a62]">
+                    <HiOutlineMail className="text-[#5ec88f]" />
+                  </div>
+                  <p>Email</p>
+                </div>
+              </div>
+              <div className="flex flex-col gap-4 p-4 rounded-2xl bg-sec">
+                <p className="p-2 pl-4 rounded-full bg-primary">{"xvalzs"}</p>
+                <p className="p-2 pl-4 rounded-full bg-primary">
+                  Balance : ${"400"}
+                </p>
+                <div className="p-4 py-8 rounded-2xl bg-primary">
+                  <h5>Transaction History</h5>
+                  <div className="pt-4">
+                    <div className="flex items-center justify-between text-center">
+                      <p>{"Netflix"}</p>
+                      <p>{"$100"}</p>
+                      <p>{"23/12/2023  20:10"}</p>
+                    </div>
+                    <div className="flex items-center justify-between text-center">
+                      <p>{"Netflix"}</p>
+                      <p>{"$100"}</p>
+                      <p>{"23/12/2023  20:10"}</p>
+                    </div>
+                    <div className="flex items-center justify-between text-center">
+                      <p>{"Netflix"}</p>
+                      <p>{"$100"}</p>
+                      <p>{"23/12/2023  20:10"}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* pagenating */}
+      <ReactPaginate
+        containerClassName="flex gap-2 text-center rounded-lg items-center outline-none justify-center items-center mt-4"
+        previousLabel={
+          <span className="p-3 border rounded-lg outline-none hover:bg-active border-sec bg-sec">
+            Prev
+          </span>
+        }
+        nextLabel={
+          <span className="p-3 border rounded-lg outline-none hover:bg-active border-sec bg-sec">
+            Next
+          </span>
+        }
+        pageCount={pageCount}
+        onPageChange={changePage}
+        pageClassName="hover:bg-active p-3 rounded-lg outline-none"
+        disabledClassName={"bg-transparent outline-none"}
+        activeLinkClassName={"p-3 bg-active rounded-lg outline-none"}
+      />
+    </div>
+  );
+};
+
+export default UsdWithdraw;

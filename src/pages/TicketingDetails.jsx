90b6525f6b77f67128ef0fefa6d7826bfc3faf8e
Oke Babatunde <tundejosilnet@gmail.com>
2023-08-10 23:48:08 +0100

    ticketing page

--- src/pages/TicketingDetails.jsx
+++ src/pages/TicketingDetails.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { styles } from "../styles";
 import { Link } from "react-router-dom";
 import { TiArrowBackOutline } from "react-icons/ti";
 
+const navLinks = [
+  {
+    title: "reply",
+  },
+  {
+    title: "forward",
+  },
+  {
+    title: "resolved",
+  },
+  {
+    title: "note*",
+  },
+];
+
 const TicketingDetails = () => {
+  const [active, setActive] = useState("");
+
   return (
     <div>
       <div className="relative mb-4">
@@ -14,6 +31,31 @@ const TicketingDetails = () => {
           <TiArrowBackOutline />
         </Link>
         <p className={`${styles.topic} mb-0 `}>ticket details</p>
+      </div>
+      <div>
+        <div className="flex text-center bg-[#282C4A] p-2 rounded-full">
+          {navLinks.map((nav, index) => (
+            <div
+              className="flex items-center w-full mx-2 capitalize "
+              key={index}
+            >
+              <Link
+                className={`${
+                  active === nav.title ? "bg-active" : "bg-purple"
+                } w-full p-2 rounded-full hover:bg-active duration-300`}
+                onClick={() => setActive(nav.title)}
+              >
+                {nav.title}
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div className="bg-white round-full">
+          <div>
+            <h1></h1>
+            <h2></h2>
+          </div>
+        </div>
       </div>
     </div>
   );
